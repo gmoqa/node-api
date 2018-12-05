28bf082c320556986cdabfb72c93ffe535b1169d
Guillermo Quinteros <gu.quinteros@gmail.com>
2018-12-05 14:18:46 -0300

    Add cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').load()
 const express = require('express')
+const cors = require('cors')
 const compression = require('compression')
 const bodyParser = require('body-parser')
 const errorHandler = require('errorhandler')
@@ -12,17 +13,15 @@ const HOST = config.host
 
 const app = express()
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(require('express-status-monitor')())
+app.use(compression())
 
 if (process.env.ENV === 'dev') {
 	app.use(require('morgan')('dev'))
 	app.use(errorHandler())
-}
-
-if (process.env.ENV === 'prod') {
-	app.use(require('express-status-monitor')())
-	app.use(compression())
 }
 
 app.use('/', router)
